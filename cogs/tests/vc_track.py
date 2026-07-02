7c603564804714d6a2f7c24e7b3719d35f57c43a
Miytko <miytko.dev@gmail.com>
2026-07-02 21:54:37 +0300

    moved vc track log from .txt to .csv

--- cogs/tests/vc_track.py
+++ cogs/tests/vc_track.py
@@ -2,11 +2,12 @@ from discord.ext import commands
 import discord
 import datetime
 import os
+import csv
 from pathlib import Path
 
 
 MAIN_PATH = Path(__file__).resolve().parent.parent.parent
-DB_FILE = os.path.join(MAIN_PATH, "db/log.txt")
+DB_FILE = os.path.join(MAIN_PATH, "db/vc_activity.csv")
 
 
 class VoiceTracker(commands.Cog):
@@ -16,23 +17,52 @@ class VoiceTracker(commands.Cog):
 
     @commands.Cog.listener()
     async def on_voice_state_update(self, member: discord.Member, before: discord.VoiceState, after: discord.VoiceState):
-        message = f"Something else happened. Error? member: {member}, before: {before}, after: {after}"
+        # message = f"Something else happened. Error? member: {member}, before: {before}, after: {after}"
         timestamp = datetime.datetime.now()
         time_txt = timestamp.strftime("[%Y-%m-%d %H:%M:%S] ")
 
+        fields = ["date", "user", "action",
+                "previous_server", "previous_channel",
+                "current_server", "current_channel"]
+
+
+        user = member
+        action = ""
+        previous_server = ""
+        previous_channel = ""
+        current_server = ""
+        current_channel = ""
+
         if before.channel is None and after.channel is not None:
-            message = f"{member} has joined {after.channel} ({after.channel.guild})"
+            action = "join"
+            current_server = after.channel.guild
+            current_channel = after.channel
+            # message = f"{member} has joined {after.channel} ({after.channel.guild})"
 
         if before.channel is not None and after.channel is None:
-            message = f"{member} has left {before.channel} ({before.channel.guild})"
+            action = "leave"
+            previous_server = before.channel.guild
+            previous_channel = before.channel
+            # message = f"{member} has left {before.channel} ({before.channel.guild})"
 
         if before.channel is not None and after.channel is not None:
-            message = f"{member} has switched from {before.channel} ({before.channel.guild}) " \
-                    f"to {after.channel} ({after.channel.guild})"
+            action = "switch"
+            current_server = after.channel.guild
+            current_channel = after.channel
+            previous_server = before.channel.guild
+            previous_channel = before.channel
+            # message = f"{member} has switched from {before.channel} ({before.channel.guild}) " \
+            #         f"to {after.channel} ({after.channel.guild})"
+
+        data = [
+            [time_txt, user, action,
+            previous_server, previous_channel,
+            current_server, current_channel]
+        ]
             
-        log_msg = time_txt + message + "\n"
-        with open(DB_FILE, "a") as f:
-            f.write(log_msg)
+        with open(DB_FILE, "a", newline="") as f:
+            writer = csv.writer(f)
+            writer.writerows(data)
 
 
 async def setup(bot):
