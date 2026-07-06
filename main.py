e7e9a0601f4a0ff8e8928aef3673412bb0375ac9
Miytko <miytko.dev@gmail.com>
2026-07-07 00:24:16 +0300

    command to see your vc time (needs optimizations)

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from discord_token import DISCORD_TOKEN
 from discord.ext import commands
 import discord
+import vc_utils
 
 
 EXTENSIONS = ["cogs.tests.vc_track"]
@@ -25,9 +26,18 @@ async def hello(ctx):
     await ctx.send("hi")
 
 
+@bot.tree.command(name="vctime", description="See how much time you've spent in vc")
+async def vctime(interaction: discord.Interaction):
+    total_minutes = vc_utils.process_log()
+    msg = f"Total time spent in vc: {total_minutes[interaction.user]}"
+    await interaction.response.send_message(msg)
+
+
 @bot.event
 async def on_ready():
     print(f"Logged in as {bot.user}")
+    await bot.tree.sync()
+    print("syncd")
 
 
 if __name__ == "__main__":
---
+++ vc_utils.py
@@ -0,0 +1,79 @@
+import datetime
+import csv
+
+def process_csv_row(row: list):
+
+    date_string = row[0]
+    date = datetime.datetime.strptime(date_string, "%Y-%m-%d %H:%M:%S")
+    
+    return {
+        "action": row[2],
+        "date": date,
+        "user": row[1],
+        "before": {
+            "channel": row[4],
+            "server": row[3]
+        },
+        "after": {
+            "channel": row[6],
+            "server": row[5]
+        }
+    }
+
+
+def process_log():
+
+    active_sessions = {}
+    total_time = {}
+
+    with open("db/vc_activity.csv", mode="r", encoding="UTF-8") as f:
+        reader = csv.reader(f)
+        next(reader) # skip header
+
+        for row in reader:
+
+            entry = process_csv_row(row)
+
+            user = entry["user"]
+
+            if user not in active_sessions:
+                active_sessions[user] = {}
+            if user not in total_time:
+                total_time[user] = 0
+            
+            if entry["action"] == "join":
+                active_sessions[user]["channel"] = entry["after"]
+                active_sessions[user]["status"] = "unmuted"
+                active_sessions[user]["date"] = entry["date"]
+            
+            elif entry["action"] == "leave":
+                if entry["before"] == active_sessions[user]["channel"]:
+                    time_spent = (entry["date"] - active_sessions[user]["date"]).total_seconds()
+                    total_time[user] += time_spent
+                    del active_sessions[user]
+            
+            elif entry["action"] == "switch":
+                time_spent = (entry["date"] - active_sessions[user]["date"]).total_seconds()
+                total_time[user] += time_spent
+                active_sessions[user]["date"] = entry["date"]
+
+                if entry["before"] == entry["after"]:
+                    if entry["after"] == active_sessions[user]["channel"]:
+                        if active_sessions[user]["status"] == "unmuted":
+                            active_sessions[user]["status"] = "muted"
+                        else:
+                            active_sessions[user]["status"] = "unmuted"
+                
+
+        total_minutes = {}
+        for user, seconds in total_time.items():
+            total_minutes[user] = round(seconds / 60)
+
+        return total_minutes
+
+        # sorted_hours = dict(sorted(total_minutes.items(), key=lambda item: item[1], reverse=True))
+        # print(json.dumps(sorted_hours, indent=4))
+
+
+if __name__ == "__main__":
+    process_log()
