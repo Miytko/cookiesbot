40dedf4282edcf658acecea375369ac14a56563f
Miytko <miytko.dev@gmail.com>
2026-07-23 22:12:37 +0300

    vcleaderboard now tells the rank of the user who invoked it, also made code a bit cleaner

--- main.py
+++ main.py
@@ -34,22 +34,35 @@ async def vctime(interaction: discord.Interaction):
     await interaction.response.send_message(msg)
 
 
+def format_time_msg(minutes):
+    return f"{math.floor(minutes/60)}h {minutes%60:02}m"
+
+
 @bot.tree.command(name="vcleaderboard", description="Top 5 users by total time spent in vc")
 async def vcleaderboard(interaction: discord.Interaction):
     total_minutes = vc_utils.process_log()
-    sorted_hours = dict(sorted(total_minutes.items(), key=lambda item: item[1], reverse=True))
+    sorted_hours = sorted(total_minutes.items(), key=lambda item: item[1], reverse=True)
 
     msg = f"Top 5 unemployed:\n"
 
-    index = 0
-    for user, value in sorted_hours.items():
+    invoker_name = interaction.user.name
+    invoker_rank = None
+    invoker_minutes = total_minutes.get(invoker_name)
+    
+
+    for index, (user, minutes) in enumerate(sorted_hours):
+        rank = index + 1
+
+        if user == invoker_name:
+            invoker_rank == rank
+
         if index < 5:
-            hours = math.floor(value / 60)
-            usermsg = f"{index+1}. {user} - {hours}h{value%60:02}m\n"
-            msg += usermsg
-            index += 1
-        else:
-            break
+            msg += f"{rank}. {user} - {format_time_msg(minutes)}\n"
+
+
+    if invoker_rank is not None and invoker_rank > 5:
+        msg += f"...{invoker_rank}. {invoker_name} - {format_time_msg(invoker_minutes)}\n"
+
     
     await interaction.response.send_message(msg)
 
